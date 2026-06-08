a71385fd81fef0805271768e2e463c400ffe755b
Ushani Perera <ushaniprarthana593@gmail.com>
2026-06-08 11:13:35 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,6 @@ const Navbar = () => {
             aria-label="Urban Harvest Hub Home"
           >
             <img src={MainLogo} alt="Urban Harvest Hub Logo" width="40" height="40" className="h-10 w-10 object-contain" />
-            <span className="hidden sm:inline">Urban Harvest Hub</span>
           </Link>
 
           {/* Desktop Navigation */}
